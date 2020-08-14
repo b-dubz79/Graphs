e47eb0b06a97af200c8a445399bafffdfbb25564
Brian Wardwell <bwardwell79@gmail.com>
2020-08-14 10:47:12 -0700

    finished Sprint MVP

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -6,7 +6,6 @@ import random
 from ast import literal_eval
 from util import Stack, Queue
 
-
 # Load world
 world = World()
 
@@ -21,6 +20,7 @@ map_file = "maps/test_cross.txt"
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
+print('room graph', room_graph)
 
 # Print an ASCII map
 world.print_rooms()
@@ -29,23 +29,62 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+# traversal_path will be filled in with the directions, literally w, e, n, s
 traversal_path = []
-
+# return path will contain cardinal directions that will lead us back to where we started
+# this will be useful when player hits leaf node but hasn't visited all rooms yet
+return_path = []
+# dictionary that stores all visited rooms. Key is room id and value is directions of exits.
 visited = {}
-# start at room 0
-# find possible exits
-visited[player.current_room.id] = player.current_room.get_exits()
-print('visited', visited)
-# while length of visited doesn't equal length of room_graph
-while len(visited) != len(room_graph):
-    # if room that we moved to isn't in dict
-    if 
-    # if we enter the last room (no more options)
-    # else: if we get to dead end but there's another way (need to go back)
-# pick a direction and log path
+# This gives us the opposite of the direction we just traveled.  We will put this opposite value in the return-path list.
+reverse_direction = {'w': 'e', 'e': 'w', 'n': 's', 's': 'n'}
 
-# visit every room
-# put the path in the traversal_path list
+# start at room 0 and find possible exits
+visited[player.current_room.id] = player.current_room.get_exits()
+print('initial visited', visited)
+# if length of visited{} is equal to length of room_graph, then all rooms have been visited
+while len(visited) < len(room_graph):
+    print('length of visited', len(visited))
+    print('length', len(room_graph))
+    print('updated visited', visited)
+     # if room that we moved to isn't in dict
+    print('current room', player.current_room.id)
+    # if current room is not in visited dict
+    if player.current_room.id not in visited:
+        print('entering new room', player.current_room.id)
+        # add key/value of current room/possible exits to visited dict
+        visited[player.current_room.id] = player.current_room.get_exits()
+        # set variable to the value of the last direction the player moved
+        remove_prev = return_path[-1]
+        print('remove', remove_prev)
+        print('return path', return_path) 
+        # removes the previous direction from the possible exits for the current room
+        visited[player.current_room.id].remove(remove_prev)
+    
+    # else: if we get to dead end but there's another way (need to go back)
+    # if length of visited at current room is 0, then we have no new options
+    elif len(visited[player.current_room.id]) == 0:
+        remove_prev = return_path[-1]
+        print('return path', return_path)
+        return_path.pop()
+        traversal_path.append(remove_prev)
+        player.travel(remove_prev)
+
+    # this is our general move logic
+    else:
+        print('ELSE')
+        # pop off last direction in visited dict for current room, and go there next
+        # ex: if dict shows {0: ['n', 's', 'w', 'e']}, then we pop 'e' off and go that way
+        choice = visited[player.current_room.id].pop()
+        print('choice', choice)
+        print('visited 2', visited[player.current_room.id])
+        # add the direction to the traversal_path
+        traversal_path.append(choice)
+        # add the opposite of the choice to the return_path so we can turn around if needed
+        return_path.append(reverse_direction[choice])
+        print('reverse choice', reverse_direction[choice])
+        # move player using choice variable
+        player.travel(choice)
 
 
 
